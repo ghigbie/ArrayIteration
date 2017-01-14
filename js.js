84209387492284d38c73e5b5f9252b0638e0ce65
George Higbie <gehigbie@gmail.com>
2017-01-13 23:43:02 -0700

    added loops to file

--- js.js
+++ js.js
@@ -1,1 +1,12 @@
 //this file was created in the terminal
+//for each loop   arr.forEach(someFucntion)
+
+var colors = ["red", "orange", "yellow", "green", "blue"];
+
+colors.forEach(function(item){
+	console.log(item);
+});
+
+for(var a = 0; a < colors.length; a++){
+	console.log(colors[a]);
+}
