24f147cab1b0e795367960630e3fb3b63cd41bbc
George Higbie <gehigbie@gmail.com>
2017-01-13 23:57:14 -0700

    added elements to html and functions to JS file

--- js.js
+++ js.js
@@ -3,10 +3,19 @@
 
 var colors = ["red", "orange", "yellow", "green", "blue"];
 
-colors.forEach(function(item){
-	console.log(item);
-});
+colors.forEach(function(item){  //can be annonymous function
+	console.log(`My favorite color is ${item}`);      //item is a place holder, call it whatever you want
+});  //item holds the value for whatever is in the array
 
 for(var a = 0; a < colors.length; a++){
 	console.log(colors[a]);
 }
+
+function printColor(color){
+	console.log("///////////");
+	console.log(color);
+	console.lgo("///////////");
+}
+
+colors.forEach(printColor);
+
